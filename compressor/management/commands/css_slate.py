62e5e56325adb0ab79df0d0eba5bde5e40bab075
Aram Dulyan <aram00@gmail.com>
2010-08-30 23:15:20 +1000

    Added a "css_slate" management command for "slating" the compileable CSS files, i.e. compiling them and setting the compiled files' permissions to writeable by all. Alternately, the --chown and/or --chgrp options can be used to just change ownership of the compiled files instead.

---
+++ compressor/management/commands/css_slate.py
@@ -0,0 +1,82 @@
+import os
+from operator import concat
+from optparse import make_option, OptionError
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.core.exceptions import ImproperlyConfigured
+
+from compressor import CssCompressor
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--chown', action='store', dest='chown', help='Change ownership of generated CSS files to specified user, instead of changing permissions. Can be combined with chgrp.'),
+        make_option('--chgrp', action='store', dest='chgrp', help='Change ownership of generated CSS files to specified group, instead of changing permissions. Can be combined with chown.'),
+        )
+    help = 'Compile CSS files and make the generated files writeable for future compilations.'
+
+    def handle(self, *args, **options):
+        # Passing -1 to chown leaves the ownership unchanged, hence the default
+        uid = -1
+        gid = -1
+        chown = options.get('chown', None)
+        chgrp = options.get('chgrp', None)
+        verbosity = int(options.get('verbosity', 1))
+        if chown or chgrp:
+            # pwd is only available on POSIX-compliant systems
+            try:
+                import pwd
+            except ImportError:
+                raise OptionError('Ownership changes are not supported by your operating system.', '--chown')
+            try:
+                if chown:
+                    uid = pwd.getpwnam(chown).pw_uid
+                if chgrp:
+                    gid = pwd.getpwnam(chgrp).pw_gid
+            except (KeyError, TypeError):
+                raise OptionError('The specified username "%s" does not exist or is invalid.' % chown, '--chown')
+        if not hasattr(os, 'chmod'):
+            raise NotImplementedError('Permission changes are not supported by your operating system')
+        if not hasattr(settings, 'COMPILER_FORMATS') or not settings.COMPILER_FORMATS:
+            raise ImproperlyConfigured('COMPILER_FORMATS not specified in settings.')
+        
+        if verbosity:
+            print 'Looking for slateable CSS files in %s' % settings.MEDIA_ROOT
+        
+        # Find all files in MEDIA_ROOT that have a COMPILER_FORMATS-supported
+        # extension, and return them as a list of (full path to file without
+        # extension, extension) tuples.
+        files_to_compile = reduce(
+            concat,
+            map(
+                lambda(root, dirs, files): map(
+                    lambda (css_file, extension): ('%s/%s' % (root, css_file), extension),
+                    filter(
+                        bool,
+                        map(
+                            lambda media_file: reduce(
+                                lambda prev, next: prev or media_file.endswith(next) \
+                                    and (media_file[:-len(next)], next),
+                                settings.COMPILER_FORMATS.keys(),
+                                False),
+                            files))),
+                os.walk(settings.MEDIA_ROOT)))
+        
+        if verbosity:
+            print 'Found %s files to be slated...' % len(files_to_compile)
+        
+        for filename, extension in files_to_compile:
+            if verbosity > 1:
+                print 'Compiling %s%s' % (filename, extension)
+            CssCompressor.compile(filename, settings.COMPILER_FORMATS[extension])
+            css_file = '%s.css' % filename
+            if chown or chgrp:
+                # Change file ownership to specified group and/or user
+                os.chown(css_file, uid, gid)
+                # Make sure owner can write and everyone can read
+                os.chmod(css_file, 0644)
+            else:
+                # Allow everyone to read and write
+                os.chmod(css_file, 0666)
+        
+        if verbosity:
+            print 'Finished slating.'
