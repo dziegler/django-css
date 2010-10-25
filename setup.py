0a0d7124b8136ff6b078958a13d241979c2c32df
dziegler <david.ziegler@gmail.com>
2010-10-24 22:51:02 -0400

    bump version number to 2.3.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = read('README.rst')
 
 setup(
     name = "django-css",
-    version = "2.3.0",
+    version = "2.3.1",
     description='django-css provides an easy way to use CSS compilers with Django projects, and an automated system for compressing CSS and JavaScript files',
     url = 'http://github.com/dziegler/django-css',
     license = 'BSD',
